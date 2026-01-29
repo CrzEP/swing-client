300df001aaebb2c34741db03db3d86e2445b5801
lingui <linguiduan@gmail.com>
2026-01-29 11:42:41 +0800

    移除一些不必要的参数

--- src/main/java/com/dlg/controller/ApiController.java
+++ src/main/java/com/dlg/controller/ApiController.java
@@ -6,10 +6,8 @@ import com.dlg.util.HttpUtil;
 public class ApiController {
 
     public static String getProductInfo(){
-        String url = "https://app.servicepoints.nl/api/v2/get-products";
-        String param = "{\n" +
-                "    \"is_quotation_product\":2\n" +
-                "}";
+        String url = "";
+        String param = "";
         JSONObject paramJson = JSONObject.parseObject(param);
         JSONObject jsonObject = HttpUtil.post(url, paramJson);
         return jsonObject.toJSONString();
